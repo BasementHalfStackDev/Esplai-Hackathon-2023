75413589f58027baf7163efdd9acbf4a965434b6
Josep Mª Pallàs Batalla <jpb_montblanc@hotmail.com>
2023-05-16 17:41:58 +0200

    First commit

---
+++ Hackathon_JPallas/src/main/java/UserLogin/HackathonJPallasApplication.java
@@ -0,0 +1,13 @@
+package UserLogin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HackathonJPallasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HackathonJPallasApplication.class, args);
+	}
+
+}
